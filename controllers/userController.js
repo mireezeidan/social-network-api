a396b34bebf41fd448ed8d34400fb891f731539d
Miree Zeidan <mireezeidan@gmail.com>
2023-03-07 20:10:11 -0600

    finished thought model and user controller

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteSingleUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $set: req.body }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Add a freind
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user found with that ID :(" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove a friend
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { runValidators: true, new: true })
+      .then((friend) => (!friend ? res.status(404).json({ message: "No friend found with that ID :(" }) : res.json(friend)))
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { getUsers, getSingleUser, createUser, deleteSingleUser, updateUser, addFriend, removeFriend } = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteSingleUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
